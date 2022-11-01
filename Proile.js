f423340eae0ec94bb1d861c75a4dc719e097ac00
UrviVassisht15 <urvivasisht74@gmail.com>
2022-11-01 13:22:34 +0530

    Profile component

---
+++ Proile.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Profile=()=>
+{
+
+    const mystyle = {
+        color: "white",
+        backgroundColor: "white",
+        padding: "10px",
+        border:"1px",
+        height:"130px",
+        borderRadius:"70px",
+        width:"150px"
+      };
+
+      
+      
+    return(
+
+        <>
+        
+<p>Home / Facu/ty / Assistant Pro lessor / Anjali Mehta</p>
+
+<div className='flex1'>
+<div><img src="../images/download.jpg" style={mystyle}/></div>
+  <div style={{marginLeft:"30px"}}>
+<p style={{fontSize:"18px"}}>Anjali Mehta</p>
+<h2 style={{fontSize:"14px"}}>Assistant Professor</h2>
+<table cellPadding={"10px"}>
+
+  <tr>
+    <td><b>Department</b></td>
+ <td><b>Papers</b></td>
+ <td> <b>Grade</b></td>
+ <td> <b>Education</b></td>
+ </tr>
+
+ <tr>
+    <td>CSE</td>
+ <td>56</td>
+ <td>Ultra Pro Max	Btech</td>		 
+ <td> IIT Mumbai</td>
+ </tr>
+</table>
+
+</div>
+</div>
+
+
+        </>
+    );
+}
+
+export default Profile;
